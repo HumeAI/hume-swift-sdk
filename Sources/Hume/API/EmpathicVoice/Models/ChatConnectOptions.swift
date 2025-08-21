35919540e0ea24cad011d1603245dbb944a993ed
Richard Marmorstein <twitchard@users.noreply.github.com>
2025-08-21 12:55:20 -0700

    Support voice id (#29)
    * Support voice id
    
    * Introduce complete ConnectOptions
    
    * format
    
    * improvements
    
    * wip
    
    * format

---
+++ Sources/Hume/API/EmpathicVoice/Models/ChatConnectOptions.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+public struct ChatConnectOptions: Codable {
+  public var configId: String?
+  public var configVersion: String?
+  public var resumedChatGroupId: String?
+  public var voiceId: String?
+  public var verboseTranscription: Bool?
+  public var eventLimit: Int?
+
+  public init(
+    configId: String? = nil,
+    configVersion: String? = nil,
+    resumedChatGroupId: String? = nil,
+    voiceId: String? = nil,
+    verboseTranscription: Bool? = nil,
+    eventLimit: Int? = nil
+  ) {
+    self.configId = configId
+    self.configVersion = configVersion
+    self.resumedChatGroupId = resumedChatGroupId
+    self.voiceId = voiceId
+    self.verboseTranscription = verboseTranscription
+    self.eventLimit = eventLimit
+  }
+}
