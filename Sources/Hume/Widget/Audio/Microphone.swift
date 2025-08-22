0819cc30dfc61e2c622c7abdeeca5b12fa5f9d60
Chris Manahan <chrismanahan@gmail.com>
2025-08-22 12:37:38 -0400

    fix input format for mic (#33)

--- Sources/Hume/Widget/Audio/Microphone.swift
+++ Sources/Hume/Widget/Audio/Microphone.swift
@@ -43,7 +43,7 @@ internal final class Microphone: NSObject {
 
     // initialize audio engine and nodes
     inputNode = audioEngine.inputNode
-    inputFormat = inputNode.inputFormat(forBus: 0)
+    inputFormat = inputNode.outputFormat(forBus: 0)
     let outputNode = audioEngine.outputNode
 
     // sink node to listen for realtime input
