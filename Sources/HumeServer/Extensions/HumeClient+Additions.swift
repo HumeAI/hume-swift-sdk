5e1d9a022b24561fb81f2c32cdff58c74199cbd1
twitchard <richard.marmorstein@gmail.com>
2025-08-26 11:38:07 -0700

    Restore this

---
+++ Sources/HumeServer/Extensions/HumeClient+Additions.swift
@@ -0,0 +1,16 @@
+//
+//  HumeClient+Additions.swift
+//  Hume
+//
+//  Created by Chris on 8/22/25.
+//
+
+import Hume
+
+#if HUME_SERVER
+
+  extension HumeClient {
+    static var isRunningOnServer: Bool { true }
+
+  }
+#endif
