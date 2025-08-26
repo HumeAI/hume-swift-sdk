5649745ffc4f7c1c22d3bfd502916b22a50a8db7
twitchard <richard.marmorstein@gmail.com>
2025-08-26 10:46:22 -0700

    wip

---
+++ Sources/HumeServer/HumeServer.swift
@@ -0,0 +1,11 @@
+//
+//  HumeServer.swift
+//  Hume
+//
+//  Server-side module for Hume SDK
+//
+
+import Foundation
+
+// This module enables server-side functionality
+// Import this module to access server-only convenience methods
